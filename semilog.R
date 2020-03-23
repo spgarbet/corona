9ea832882aec2335c54b3ee88cb4df3b9ca5f481
Shawn Garbett <shawn@garbett.org>
2020-03-23 11:27:22 -0500

    Fixed minor bug in semilog code

--- semilog.R
+++ semilog.R
@@ -10,7 +10,7 @@ semilog    <- function(data, ...)
     points(doy[deaths > 0], deaths[deaths > 0], pch=4)
     
     y1 <- list(...)[['ylim']]
-    if(is.null(y1)) y1 <- range(us_data$cases[cases >0])
+    if(is.null(y1)) y1 <- range(cases[cases >0])
     y1 <- floor(log10(y1))
     pow <- seq(y1[1], y1[2]+1)
     ticksat <- as.vector(sapply(pow, function(p) (1:10)*10^p))
